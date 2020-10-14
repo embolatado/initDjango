1c28bc6e345175c8e98606e15d9e07405dc7898b
embolatado <embolatado@gmail.com>
2020-10-14 16:29:39 -0500

    Primera plantilla creada

--- vid05/vid05/urls.py
+++ vid05/vid05/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from vid05.views import saludo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hola/', saludo),
 ]
---
+++ vid05/vid05/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+import datetime
+from django.template import Template, Context
+
+def saludo(request):
+
+    doc_externo = open(
+        '/vid05/vid05/plantillas/miplantilla.html')
+
+    # 1. 
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+
+    contexto = Context()
+
+    # RENDERIZAR 
+    documento =  plantilla.render(contexto)
+
+    return HttpResponse(documento)
