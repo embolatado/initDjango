c9a33135778b833f490cbfd495c4f4aa799c3b8f
embolatado <embolatado@gmail.com>
2020-10-14 12:55:25 -0500

    Puntuación añadida a vistas

--- vid04/vid04/views.py
+++ vid04/vid04/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
 
 def saludo(request):
-    return HttpResponse("Hola, alumnos Django")
+    return HttpResponse("¡Hola, alumnos Django!")
 
 def adios(request):
-    return HttpResponse("Hasta pronto, alumnos Django")
+    return HttpResponse("¡Hasta pronto, alumnos Django!")
 
 
