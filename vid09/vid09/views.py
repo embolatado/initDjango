e642738be6b7e2229cd911529e29a3c5d76c3d0a
embolatado <embolatado@gmail.com>
2021-04-14 11:23:16 -0500

    shortcuts render & commented all not longer needed

--- vid09/vid09/views.py
+++ vid09/vid09/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
-from django.template import Template, Context
-from django.template.loader import get_template
+# from django.http import HttpResponse
+# from django.template import Template, Context
+# from django.template.loader import get_template
+from django.shortcuts import render
 
 
 class Persona(object):
@@ -12,9 +13,9 @@ class Persona(object):
 def saludo(request):
     persona1 = Persona("Juán", "Díaz")
     
-    # CON loader
-    var_a = get_template('plantilla1.html')
-    var_b = var_a.render({"el_nombre": persona1.nombre, "el_apell": persona1.apelli.upper})
+    # CON loader Y shortcuts
+    # var_a = get_template('plantilla1.html')
+    # var_b = var_a.render({"el_nombre": persona1.nombre, "el_apell": persona1.apelli.upper})
 
-    return HttpResponse(var_b)
+    return render(request, 'plantilla1.html', {"el_nombre": persona1.nombre, "el_apell": persona1.apelli.upper})
 
