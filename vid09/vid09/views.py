51797c8194d5bbf4fa0fe92486b2e7bfb7ac9caf
embolatado <embolatado@gmail.com>
2021-04-14 09:12:15 -0500

    Import .loader and call get_template()

--- vid09/vid09/views.py
+++ vid09/vid09/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import Template, Context
-from django.template import loader
+from django.template.loader import get_template
 
 
 class Persona(object):
@@ -13,7 +13,7 @@ def saludo(request):
     persona1 = Persona("Juán", "Díaz")
     
     # CON loader
-    var_a = loader.get_template('plantilla1.html')
+    var_a = get_template('plantilla1.html')
     var_b = var_a.render({"el_nombre": persona1.nombre, "el_apell": persona1.apelli.upper})
 
     return HttpResponse(var_b)
